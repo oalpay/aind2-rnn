0073bb903f0746085556a9322447fe2cf0aede84
Osman A <osman.alpay@gmail.com>
2017-06-25 19:29:42 +0300

    submission ready

--- my_answers.py
+++ my_answers.py
@@ -12,7 +12,9 @@ def window_transform_series(series,window_size):
     # containers for input/output pairs
     X = []
     y = []
-
+    for window_end in range(window_size,len(series)):
+        X.append(series[window_end-window_size:window_end])
+        y.append(series[window_end])
     # reshape each 
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
@@ -23,15 +25,37 @@ def window_transform_series(series,window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(step_size, window_size):
-    pass
+    from keras.models import Sequential
+    from keras.layers import Dense
+    from keras.layers import LSTM
+    import keras
 
+    # given - fix random seed - so we can all reproduce the same results on our default time series
+    np.random.seed(0)
+
+    # TODO: build an RNN to perform regression on our time series input/output data
+    model = Sequential()
+    model.add(LSTM(5, input_shape=(window_size, 1)))
+    model.add(Dense(1))
+
+
+    # build model using keras documentation recommended optimizer initialization
+    optimizer = keras.optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
+
+    # compile the model
+    model.compile(loss='mean_squared_error', optimizer=optimizer)
+
+    model.summary()
 
 ### TODO: list all unique characters in the text and remove any non-english ones
 def clean_text(text):
-    # find all unique characters in the text
-
-
     # remove as many non-english characters and character sequences as you can 
+    import string
+    remove_punctuation_map = dict((ord(char), ' ') for char in (string.punctuation + '\n'))
+    text = text.translate(remove_punctuation_map)
+
+    # shorten any extra dead space created above
+    text = text.replace('  ',' ')
 
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
@@ -39,7 +63,8 @@ def window_transform_text(text,window_size,step_size):
     # containers for input/output pairs
     inputs = []
     outputs = []
-    
-
+    for window_end in range(window_size,len(text),step_size):
+        inputs.append(text[window_end-window_size:window_end])
+        outputs.append(text[window_end])     
     
     return inputs,outputs
